66265df5d309565a470c1f965e7102171c2c9730
Aghilashub2020 <aghilasrabia15@gmail.com>
2022-05-04 19:01:29 -0400

    delete route complete

--- views/show.jsx
+++ views/show.jsx
@@ -1,7 +1,7 @@
 const React = require("react");
 const Default = require("./layouts/Default");
 
-function Show({ bread }) {
+function Show({ bread, index }) {
   return (
     <Default>
       <h3>{bread.name}</h3>
@@ -11,11 +11,13 @@ function Show({ bread }) {
         have gluten.
       </p>
       <img src={bread.image} alt={bread.name} />
+
+      <form action={`/breads/${index}?_method=DELETE`} method="POST">
+        <input type="submit" value="DELETE" />
+      </form>
+
       <li>
         <a href="/breads">Go home</a>
-        <form action={`/breads/${index}?_method=DELETE`} method="POST">
-          <input type="submit" value="DELETE" />
-        </form>
       </li>
     </Default>
   );
