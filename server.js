a3fb0c2601fec1b13ca41e0d8988ead92fecc689
Aghilashub2020 <aghilasrabia15@gmail.com>
2022-05-09 20:24:58 -0400

    edit and update routes complete

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 // DEPENDENCIES
 const express = require("express");
 const methodOverride = require("method-override");
+const mongoose = require("mongoose");
+
 // CONFIGURATION
 require("dotenv").config();
 const PORT = process.env.PORT;
@@ -13,6 +15,14 @@ app.engine("jsx", require("express-react-views").createEngine());
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+mongoose.connect(
+  process.env.MONGO_URI,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("connected to mongo: ", process.env.MONGO_URI);
+  }
+);
+
 
 // ROUTES
 app.get("/", (req, res) => {
